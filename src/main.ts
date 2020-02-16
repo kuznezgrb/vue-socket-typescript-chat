2ce0af46a31fd9f658b064ef9df90d2b00d175f9
Denis <last.head@yandex.ru>
2020-02-17 00:01:07 +0300

    Init socket

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import Vue from 'vue';
+import VueSocketio from 'socket.io';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+Vue.use(VueSocketio, 'http://localhost:8070/');
 
 new Vue({
   render: (h) => h(App)
