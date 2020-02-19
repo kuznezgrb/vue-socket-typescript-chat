1a33f170b55d1888925d21a0b061e178c82882b0
Denis <last.head@yandex.ru>
2020-02-19 11:31:09 +0300

    Fixed the client directory assembly

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,9 +1,5 @@
 import Vue from 'vue';
-import * as VueSocketio from 'socket.io-client';
 import App from './App.vue';
-
-Vue.config.productionTip = false;
-Vue.use(VueSocketio, 'http://localhost:8070/');
 
 new Vue({
   render: (h) => h(App)
