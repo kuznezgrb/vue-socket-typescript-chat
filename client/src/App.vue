8145da569b5da48727839ff610686526aeecf485
Denis <last.head@yandex.ru>
2020-02-28 01:04:48 +0300

    Add layout

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <AppLayout />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import AppLayout from "./views/AppLayout.vue";
+import socket from "socket.io-client";
+
+export default Vue.extend({
+  name: "App",
+  components: {
+    AppLayout
+  },
+  data: () => ({
+    //
+  })
+});
+</script>
---
+++ client/src/views/AppLayout.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container__app-container">
+    <AppLeftColumn />
+    <div class="chat__body">
+      <AppHeader />
+      <ChatArea class="chat__area" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import AppLeftColumn from "@/components/AppLeftColumn.vue";
+import ChatArea from "@/components/ChatArea.vue";
+import AppHeader from "@/components/AppHeader.vue";
+
+require("../assets/blocks");
+
+export default Vue.extend({
+  name: "AppLayout",
+  components: {
+    AppLeftColumn,
+    ChatArea,
+    AppHeader
+  }
+});
+</script>
+
+<style lang="scss">
+@import "../assets/mixins";
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+:root {
+  --color-font: #35344d;
+  --color-font-menu: #cac4e3;
+  --color-link: #524bfc;
+  --color-dark-base: #5249ff;
+  --color-background-light: rgba(247, 247, 247, 0.5);
+  --font-default: "PT Sans";
+
+  --transition-ease: all 0.3s;
+  --transition-medium: all 0.7s;
+  --transition-slow: all 1.2s;
+
+  --sidebar-left-width: 26rem;
+  --sidebar-padding-left: 5rem;
+}
+
+html {
+  font-size: 5px;
+  @media #{$min768} {
+    font-size: 10px;
+  }
+  position: relative;
+}
+
+.container {
+  &__app-container {
+    background: var(--color-background-light);
+    width: 100%;
+  }
+}
+
+.chat {
+  &__area {
+    padding-left: calc(var(--sidebar-left-width) + var(--sidebar-padding-left));
+  }
+}
+</style>
