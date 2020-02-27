bd6730f987e80562d74864f941a178b9be91a77d
Denis <last.head@yandex.ru>
2020-02-28 01:03:34 +0300

    Add base components

--- client/src/components/AppHeader.vue
+++ client/src/components/AppHeader.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>
-    <div>{{ msg }}</div>
-  </div>
+  <div class="header__container">Header</div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-
-@Component
-export default class AppHeader extends Vue {
-  @Prop() private msg!: string;
-}
+import Vue from "vue";
+export default Vue.extend({
+  name: "AppHeader"
+});
 </script>
+
+<style lang="scss" scoped>
+.header {
+  &__container {
+    background: #ffffff;
+    padding-left: calc(var(--sidebar-left-width) + var(--sidebar-padding-left));
+  }
+}
+</style>
