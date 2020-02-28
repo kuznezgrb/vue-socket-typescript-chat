b3c5fee1773b5a9b44bdd9f1d63100b51af7adf2
Denis <last.head@yandex.ru>
2020-02-28 22:17:03 +0300

    Client: Add custom scss variables for vuetify

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            "vue-style-loader",
+            "css-loader",
+            {
+              loader: "sass-loader",
+              options: {
+                // This is the path to your variables
+                prependData: "@import '@/src/assets/variables.scss'"
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
 };
